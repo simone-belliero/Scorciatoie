413f304bb77dcd22b971e4e80b58f94cd3ba8414
simone-belliero <74213643+simone-belliero@users.noreply.github.com>
2026-04-28 16:34:23 +0200

    Modifica iniziale risistemazione nomi e cartelle

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.12.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -62,11 +62,11 @@ Namespace My
      Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
      Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>  _
     Friend Module MySettingsProperty
-
-        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>
-        Friend ReadOnly Property Settings() As Global.Scorciatoie_V6_3.My.MySettings 'Global.TestFileINI.My.MySettings
+        
+        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
+        Friend ReadOnly Property Settings() As Global.Scorciatoie.My.MySettings
             Get
-                Return Global.Scorciatoie_V6_3.My.MySettings.Default 'Global.TestFileINI.My.MySettings.Default
+                Return Global.Scorciatoie.My.MySettings.Default
             End Get
         End Property
     End Module
---
+++ README.md
@@ -0,0 +1,49 @@
+# Scorciatoie
+
+**Barre laterali flottanti di scorciatoie rapide per Windows**
+
+Un tool leggero e personalizzabile per avere sempre a portata di mouse le tue applicazioni, cartelle, testi fissi e clipboard multipli.
+
+![Screenshot principale](screenshot-main.png) <!-- aggiungerai lo screenshot dopo -->
+
+## ✨ Funzionalità principali
+
+- Barre laterali **flottanti** e trascinabili (una per gruppo)
+- Scorciatoie con icone personalizzate
+- **Multi-Clipboard** avanzato
+- Testi fissi rapidi
+- Gestione gruppi e colori
+- Salvataggio automatico della configurazione
+- Drag & Drop per riordinare le scorciatoie
+- Supporto multi-monitor
+
+## 📥 Download
+
+*(Prossimamente verranno aggiunte le release con installer)*
+
+## 🛠 Tecnologie
+
+- **Visual Basic .NET** (.NET 6 / Windows Forms)
+- Configurazione in file INI (presto migrata a JSON)
+
+## 📸 Screenshot
+
+*(Aggiungerai qui le immagini delle barre)*
+
+## 🚀 Come compilare
+
+1. Apri la soluzione `Scorciatoie.sln` con Visual Studio 2022 o superiore
+2. Compila (F5)
+3. L’eseguibile si trova in `bin\Debug\net6.0-windows`
+
+## 📝 Prossimi sviluppi
+
+- Passaggio a configurazione **JSON**
+- Hotkey globali (Win + Alt + ...)
+- Modalità Dark
+- Miglioramento UI/UX
+- Versione portable + installer MSIX
+
+---
+
+**Made with ❤️ by Simone Belliero**
